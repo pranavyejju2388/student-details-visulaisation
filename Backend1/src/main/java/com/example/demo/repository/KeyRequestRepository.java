bd93c75e8c5cac453ea4e6357d74ea0b5c2bbf5a
pranavyejju2388 <yejjuvenkatapranav@gmail.com>
2025-03-17 19:14:59 +0530

    updated backend StudentDV

--- Backend1/src/main/java/com/example/demo/repository/KeyRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.KeyRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KeyRequestRepository extends JpaRepository<KeyRequest, Long> {
-}
